f5874a1d411fc8452ab24d575611b044d806ab43
Jaykumar Halpati <rameshhalpati007@gmail.com>
2020-07-31 20:40:00 +0100

    Increased size of the map

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import L from 'leaflet';
-import axios from 'axios';
+import React from "react";
+import { Helmet } from "react-helmet";
+import L from "leaflet";
+import axios from "axios";
 
-import Layout from 'components/Layout';
-import Container from 'components/Container';
-import Map from 'components/Map';
-import Snippet from 'components/Snippet';
+import Layout from "components/Layout";
+import Container from "components/Container";
+import Map from "components/Map";
+import Snippet from "components/Snippet";
 
 const LOCATION = {
   lat: 38.9072,
@@ -16,8 +16,6 @@ const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
 const IndexPage = () => {
-
-
   /**
    * mapEffect
    * @description Fires a callback once the page renders
@@ -25,67 +23,57 @@ const IndexPage = () => {
    */
 
   async function mapEffect({ leafletElement: map } = {}) {
-    if( !map ) return;
+    if (!map) return;
 
     let response;
 
-    try{
-      response = await axios.get('https://disease.sh/v3/covid-19/countries');
-
-    } catch(e){
-      console.log('E', e);
+    try {
+      response = await axios.get("https://disease.sh/v3/covid-19/countries");
+    } catch (e) {
+      console.log("E", e);
       return;
-
     }
-    
+
     const { data } = response;
     const hasData = Array.isArray(data) && data.length > 0;
 
-    if ( !hasData) return;
+    if (!hasData) return;
 
     const geoJson = {
       features: data.map((country = {}) => {
-        const { countryInfo ={} } = country;
-        const {lat, long:lng } = countryInfo;
-        return{
-          type: 'Feature',
-          properties:{
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: "Feature",
+          properties: {
             ...country,
           },
           geometry: {
-            type: 'Point',
-            coordinates: [lng, lat]
-          }
-        }
+            type: "Point",
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+
+    function countryPointToLayer(feature = {}, latlng) {
+      const { properties = {} } = feature;
+      let updatedFormatted;
+      let casesString;
+
+      const { country, updated, cases, deaths, recovered } = properties;
+
+      casesString = `${cases}`;
+
+      if (cases > 1000) {
+        casesString = `${casesString.slice(0, 3)}k+`;
       }
-      )
-    }
 
+      if (updated) {
+        updatedFormatted = new Date(updated).toLocaleString();
+      }
 
-  function countryPointToLayer (feature = {}, latlng){
-    const { properties = {} } = feature;
-    let updatedFormatted;
-    let casesString;
-
-    const{
-      country,
-      updated,
-      cases,
-      deaths,
-      recovered
-    } = properties
-
-    casesString = `${cases}`;
-
-    if ( cases > 1000 ){
-      casesString = `${casesString.slice(0,3)}k+`
-    }
-
-    if ( updated ){
-      updatedFormatted = new Date(updated).toLocaleString();
-    }
-
-    const html = `
+      const html = `
     <span class="icon-marker">
       <span class="icon-marker-tooltip">
       <h2>${country}</h2>
@@ -96,30 +84,28 @@ const IndexPage = () => {
           <li><strong>Last update:</strong> ${updatedFormatted}</li>
         </ul>
       </span>
-      ${ casesString }
+      ${casesString}
     </span>
     `;
 
-    return L.marker( latlng, {
-      icon: L.divIcon({
-        className: 'icon',
-        html
-      }),
-      riseOnHover: true
+      return L.marker(latlng, {
+        icon: L.divIcon({
+          className: "icon",
+          html,
+        }),
+        riseOnHover: true,
+      });
+    }
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: countryPointToLayer,
     });
-  }
-
-    const geoJsonLayers = new L.GeoJSON(geoJson,{
-      pointToLayer: countryPointToLayer
-
-    })
     geoJsonLayers.addTo(map);
   }
 
-
   const mapSettings = {
     center: CENTER,
-    defaultBaseMap: 'OpenStreetMap',
+    defaultBaseMap: "OpenStreetMap",
     zoom: DEFAULT_ZOOM,
     mapEffect,
   };
@@ -130,10 +116,8 @@ const IndexPage = () => {
         <title>Home Page</title>
       </Helmet>
 
-      <Map {...mapSettings}>
-      </Map>
-
-       </Layout>
+      <Map {...mapSettings}></Map>
+    </Layout>
   );
 };
 
